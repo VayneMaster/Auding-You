b6b5f8ed846bd0546b79c942ccfcb40d0787225d
MasterVayne <damyvschaijk@gmail.com>
2025-07-18 11:30:36 +0200

    fixed typo

--- app/Tabs/HomeDashboard.tsx
+++ app/Tabs/HomeDashboard.tsx
@@ -53,7 +53,7 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.05,
         shadowOffset: {width: 0, height: 2},
         shadowRadius: 4,
-        elevation: 2.
+        elevation: 2,
     },
     title: {fontSize: 18, fontWeight: '600', marginBottom: 6},
     text: { fontSize: 16, marginBottom: 8},
